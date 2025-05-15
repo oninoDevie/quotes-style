aac1ecbda0a2c0046ed35fa742c40273370f639a
CristianMauricio <mauriciocastillo.2049@gmail.com>
2025-05-14 20:23:53 -0500

    Primer commit

---
+++ Desktop/AgradecimientosTorneo/code.js
@@ -0,0 +1,6 @@
+/**
+ * CH Tennis · Landing temporal
+ * Placeholder para futuras funcionalidades
+ * (analítica, lightbox, etc.).
+ */
+console.info("🏆 Landing CH Tennis 2025 cargada con fotos y videos.");
